d1950d6429aa65e86359b8b226c3abaad6f580af
zmcdonaldwright <85713533+zmcdonaldwright@users.noreply.github.com>
2021-06-29 15:50:27 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "PassGen to come soon...."
+    return render_template("PassGen.html")
