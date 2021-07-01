1ed7a7f61be0b690e18c9b9786b78d065b36796a
zmcdonaldwright <85713533+zmcdonaldwright@users.noreply.github.com>
2021-07-02 08:24:19 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from PasswordGenerator import pass_gen
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("PassGen.html", message="PassGen coming soon..............")
+    return render_template("PassGen.html", message=pass_gen())
